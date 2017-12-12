fe0bd18b6804f2a98b2f99ca03107800b604d42d
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2017-12-12 11:39:16 +0100

    Fix directory name matching pattern.

--- AutoTx/Resources/Install-NewestPackage.ps1
+++ AutoTx/Resources/Install-NewestPackage.ps1
@@ -1,5 +1,5 @@
 $PackageDir = Get-ChildItem -Directory -Name |
-    Where-Object {$_ -match 'build_*'} |
+    Where-Object {$_ -match 'build_[0-9]{4}-[0-9]{2}-[0-9]{2}_'} |
     Select-Object -Last 1
 
 $CurDir = Get-Location
