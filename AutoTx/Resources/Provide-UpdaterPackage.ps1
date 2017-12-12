923719447a4cabcb7ef91bbfde4077c2e80278fe
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2017-12-12 14:31:44 +0100

    Simple script to provide a new package for the automatic updater.
    Refers to #13

---
+++ AutoTx/Resources/Provide-UpdaterPackage.ps1
@@ -0,0 +1,44 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True)][String] $UpdaterSettings
+)
+
+try {
+    . $UpdaterSettings
+}
+catch {
+    $ex = $_.Exception.Message
+    Write-Host "Error reading settings file: '$($UpdaterSettings)' [$($ex)]"
+    Exit
+}
+
+
+$UpdateBinariesPath = "$($UpdateSourcePath)\Service\Binaries"
+$Pattern = 'build_[0-9]{4}-[0-9]{2}-[0-9]{2}_'
+
+$PackageDir = Get-ChildItem -Directory -Name |
+    Where-Object {$_ -match $Pattern} |
+    Sort-Object |
+    Select-Object -Last 1
+
+if ([string]::IsNullOrEmpty($PackageDir)) {
+    Write-Host "ERROR: couldn't find any directories matching '$($Pattern)'!"
+    Exit
+}
+
+
+try {
+    # exclude some files not be distributed:
+    $Exclude = @("ScriptsConfig.ps1", "Install-Service.ps1")
+    Copy-Item -Recurse -Force -ErrorAction Stop `
+        -Path $PackageDir `
+        -Exclude $Exclude `
+        -Destination $UpdateBinariesPath
+    Write-Host "Copied package [$($PackageDir)] to [$($UpdateBinariesPath)]."
+}
+catch {
+    $ex = $_.Exception.Message
+    Write-Host "Error copying service package: $($ex)"
+    Exit
+}
+
