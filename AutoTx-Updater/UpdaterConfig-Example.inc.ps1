3a4739d4a8e075fe1fdcbdb0adbb8131ec61abcc
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2017-12-12 17:34:15 +0100

    Update config file to recent changes.
    Refers to #17

--- AutoTx-Updater/UpdaterConfig-Example.inc.ps1
+++ AutoTx-Updater/UpdaterConfig-Example.inc.ps1
@@ -1,9 +1,10 @@
 # settings for the AutoTx Service Updater
 
-$InstallationPath = "C:\Tools\AutoTx"
 $ServiceName = "AutoTx"
+$InstallationPath = "C:\Tools\$($ServiceName)"
+$LogPath = "$($InstallationPath)"
 
-$UpdateSourcePath = "\\fileserver.mydomain.xy\share\\_AUTOTX_"
+$UpdateSourcePath = "\\fileserver.mydomain.xy\share\_AUTOTX_"
 $Pattern = 'build_[0-9]{4}-[0-9]{2}-[0-9]{2}_'
 
 $EmailFrom = "admin@mydomain.xy"
