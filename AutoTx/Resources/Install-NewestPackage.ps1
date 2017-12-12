8995790eca088ac74e567b561e50db8473201968
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2017-12-12 13:56:32 +0100

    Make package selection more robust.

--- AutoTx/Resources/Install-NewestPackage.ps1
+++ AutoTx/Resources/Install-NewestPackage.ps1
@@ -1,5 +1,6 @@
 $PackageDir = Get-ChildItem -Directory -Name |
     Where-Object {$_ -match 'build_[0-9]{4}-[0-9]{2}-[0-9]{2}_'} |
+    Sort-Object |
     Select-Object -Last 1
 
 $CurDir = Get-Location
