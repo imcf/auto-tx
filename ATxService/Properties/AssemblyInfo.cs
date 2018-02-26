50d9ab06c1175581c12983138c54a4bf44635bb1
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2018-02-26 14:11:58 +0100

    Add infrastructure to include build-time information in assembly.

--- ATxService/Properties/AssemblyInfo.cs
+++ ATxService/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AutoTx")]
+[assembly: AssemblyTitle("AutoTx Service")]
 [assembly: AssemblyDescription("Automatic Background Transfer Service")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("IMCF, Biozentrum, University of Basel")]
 [assembly: AssemblyProduct("AutoTx")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,3 +33,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyInformationalVersion(BuildDetails.GitCommitName +
+                                        " @ " + BuildDetails.GitBranch +
+                                        " (" + BuildDetails.BuildDate + ")")]
---
+++ Resources/BuildDetails.cs
@@ -0,0 +1,6 @@
+﻿public static class BuildDetails
+{
+    public const string GitCommitName = "?commit?";
+    public const string GitBranch = "?branch?";
+    public const string BuildDate = "?build time?";
+}
