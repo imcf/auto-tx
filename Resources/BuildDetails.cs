8a6631341bba45c17612620e2446aef006639e0c
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2018-02-27 17:40:00 +0100

    Update raw version of BuildDetails.cs.

--- Resources/BuildDetails.cs
+++ Resources/BuildDetails.cs
@@ -2,5 +2,8 @@
 {
     public const string GitCommitName = "?commit?";
     public const string GitBranch = "?branch?";
+    public const string GitMajor = "1";
+    public const string GitMinor = "0";
+    public const string GitPatch = "0";
     public const string BuildDate = "?build time?";
 }
