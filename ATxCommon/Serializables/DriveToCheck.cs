3c5199401bdf3c4a3c9e44c9c76cfccaede52a1e
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2018-03-07 23:28:45 +0100

    Clean up DriveToCheck class.

--- ATxCommon/Serializables/DriveToCheck.cs
+++ ATxCommon/Serializables/DriveToCheck.cs
@@ -1,6 +1,4 @@
-﻿using System.Xml.Serialization;
-
-namespace ATxCommon.Serializables
+﻿namespace ATxCommon.Serializables
 {
     /// <summary>
     /// Helper class for the nested SpaceMonitoring sections.
@@ -10,14 +8,11 @@ namespace ATxCommon.Serializables
         /// <summary>
         /// A drive name (single letter followed by a colon, e.g. "D:") to be monitored for space.
         /// </summary>
-        [XmlElement("DriveName")]
         public string DriveName { get; set; }
 
         /// <summary>
         /// Limit (in MB) of free space, lower values will trigger a notification.
         /// </summary>
-        /// Value is to be compared to DriveInfo.TotalFreeSpace, hence the same type (long).
-        [XmlElement("SpaceThreshold")]
         public long SpaceThreshold { get; set; }
     }
 }
