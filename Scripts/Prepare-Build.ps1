57d54756859a4122b67abe932d32b2ef5f89c515
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2018-03-11 16:04:10 +0100

    Another attempt to make Prepare-Build thread-safer.

--- Scripts/Prepare-Build.ps1
+++ Scripts/Prepare-Build.ps1
@@ -128,9 +128,9 @@ $DateShort = Get-Date -Format 'yyyy.MM.dd.HHmm'
 # use "-EA Ignore" to prevent build issues when VS is building multiple projects
 # at the same time that all use this script (leading to "file already in use"
 # errors), where they would all produce the same output anyway:
-$Date | Out-File $BuildDate -ErrorAction Ignore
-$CommitName | Out-File $BCommit -ErrorAction Ignore
-$ConfigurationName | Out-File $BuildConfig -ErrorAction Ignore
+$Date | Out-File -Force $BuildDate -ErrorAction Ignore
+$CommitName | Out-File -Force $BCommit -ErrorAction Ignore
+$ConfigurationName | Out-File -Force $BuildConfig -ErrorAction Ignore
 
 Write-Output $(
     "build-config: [$($ConfigurationName)]"
