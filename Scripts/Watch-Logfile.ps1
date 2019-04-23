7dd9cbb7a369c7200256457a8707a3a24440b2d5
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2019-04-23 22:13:37 +0200

    Show more of the log to roughly cover e.g. a service startup

--- Scripts/Watch-Logfile.ps1
+++ Scripts/Watch-Logfile.ps1
@@ -14,7 +14,7 @@ $LogFile = "$($ServiceDir)\var\$($env:COMPUTERNAME).$($ServiceName).log"
 
 if (Test-Path $LogFile) {
 	Write-Host "Watching logfile '$LogFile':"
-	Get-Content -Tail 20 -Wait $LogFile
+	Get-Content -Tail 200 -Wait $LogFile
 } else {
 	Write-Host "Logfile '$LogFile' doesn't exist."
 }
