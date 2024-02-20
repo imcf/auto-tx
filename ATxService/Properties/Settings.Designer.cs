ce71c8a4a0fcb67ebd473a1d634f7035778b8a2f
Michael Mell <michael.mell@unibas.ch>
2024-02-20 16:39:53 +0100

    Build project into Visual Studio 2022
    - Bump .Net framework to 4.8
    - Fix build errors after import into VS 2022

--- ATxService/Properties/Settings.Designer.cs
+++ ATxService/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ATxTray.Properties {
+namespace ATxService.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
